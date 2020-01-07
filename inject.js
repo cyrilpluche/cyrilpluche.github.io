2b9fe7436129789f1c22af143abf6f9fbcda10c1
cyrilpluche <cyril@vauban.io>
2020-01-07 11:31:03 +0000

    test 1

--- inject.js
+++ inject.js
@@ -4,11 +4,11 @@ $(document).ready(function(){
 	$.get("https://cyrilpluche.github.io/", function( html ) {
 		// my_var contains whatever that request returned
 		let structurer = html
-		structurer = structurer.replace(`<title>vauban-static</title>`)
-		structurer = structurer.replace(`<link data-n-head="1"rel="icon"type="imagex-icon"href="favicon.ico">`)
-		structurer = structurer.replace(`<meta data-n-head="1" charset="utf-8">`)
-		structurer = structurer.replace(`<meta data-n-head="1" name="viewport" content="width=device-width initial-scale=1">`)
-		structurer = structurer.replace(`<meta data-n-head="1" data-hid="description" name="description" content="static components of vauban">`)
+		// structurer = structurer.replace(`<title>vauban-static</title>`)
+		// structurer = structurer.replace(`<link data-n-head="1"rel="icon"type="imagex-icon"href="favicon.ico">`)
+		// structurer = structurer.replace(`<meta data-n-head="1" charset="utf-8">`)
+		// structurer = structurer.replace(`<meta data-n-head="1" name="viewport" content="width=device-width initial-scale=1">`)
+		// structurer = structurer.replace(`<meta data-n-head="1" data-hid="description" name="description" content="static components of vauban">`)
 		console.log(structurer)
 		document.getElementById("vauban-structurer").innerHTML = structurer
 		document.getElementById("vauban-structurer").style["display"] = "block";
