6767ac2dc01ee0c60924a622011205c572e89d4e
cyrilpluche <cyril@vauban.io>
2020-01-07 11:35:14 +0000

    custom insertion

--- inject.js
+++ inject.js
@@ -3,13 +3,15 @@ $(document).ready(function(){
 	// $('#vauban-structurer').load("https://cyrilpluche.github.io/");
 	$.get("https://cyrilpluche.github.io/", function( html ) {
 		// my_var contains whatever that request returned
-		let structurer = html 
+		let structurer = html.split("<body>")[1].split("</body>")[0] 
+		let header = html.split("<head>")[1].split("</head>")[0]
 		// structurer = structurer.replace(`<title>vauban-static</title>`)
 		// structurer = structurer.replace(`<link data-n-head="1"rel="icon"type="imagex-icon"href="favicon.ico">`)
 		// structurer = structurer.replace(`<meta data-n-head="1" charset="utf-8">`)
 		// structurer = structurer.replace(`<meta data-n-head="1" name="viewport" content="width=device-width initial-scale=1">`)
 		// structurer = structurer.replace(`<meta data-n-head="1" data-hid="description" name="description" content="static components of vauban">`)
 		console.log(structurer)
+		document.getElementsByTagName("head").innerHTML += header
 		document.getElementById("vauban-structurer").innerHTML = structurer
 		document.getElementById("vauban-structurer").style["display"] = "block";
 	});
