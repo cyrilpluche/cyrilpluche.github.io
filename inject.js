f7f51a41016c0f3ee7dab6bbb96df954cc2c87e0
cyrilpluche <cyril@vauban.io>
2020-01-07 11:20:58 +0000

    new method

--- inject.js
+++ inject.js
@@ -1,15 +1,19 @@
 $(document).ready(function(){
 	document.getElementById("vauban-structurer").style["display"] = "none";
-	$('#vauban-structurer').load("https://cyrilpluche.github.io/");
-	setTimeout(function(){
+	// $('#vauban-structurer').load("https://cyrilpluche.github.io/");
+	$.get("https://cyrilpluche.github.io/", function( html ) {
+		// my_var contains whatever that request returned
+		let structurer = html
+		structurer = structurer.replace(`<title>vauban-static</title>`)
+		structurer = structurer.replace(`<link data-n-head="1"rel="icon"type="imagex-icon"href="favicon.ico">`)
+		structurer = structurer.replace(`<meta data-n-head="1" charset="utf-8">`)
+		structurer = structurer.replace(`<meta data-n-head="1" name="viewport" content="width=device-width initial-scale=1">`)
+		structurer = structurer.replace(`<meta data-n-head="1" data-hid="description" name="description" content="static components of vauban">`)
+		console.log(structurer)
+		document.getElementById("vauban-structurer").innerHTML = structurer
 		document.getElementById("vauban-structurer").style["display"] = "block";
-		let header = document.head.innerHTML
-
-		header = header.replace(`<title>vauban-static</title>`)
-		header = header.replace(`<link data-n-head="1"rel="icon"type="imagex-icon"href="favicon.ico">`)
-		header = header.replace(`<meta data-n-head="1" charset="utf-8">`)
-		header = header.replace(`<meta data-n-head="1" name="viewport" content="width=device-width initial-scale=1">`)
-		header = header.replace(`<meta data-n-head="1" data-hid="description" name="description" content="static components of vauban">`)
-		document.head.innerHTML = header
-	},1500)  
+	});
+	// setTimeout(function(){
+	// 	document.getElementById("vauban-structurer").style["display"] = "block";
+	// },1500)  
 });
