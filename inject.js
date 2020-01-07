cc6736a535e6d124fe5b43b1d65075ec995b666a
cyrilpluche <cyril@vauban.io>
2020-01-07 11:01:43 +0000

    scipt change

--- inject.js
+++ inject.js
@@ -4,8 +4,12 @@ $(document).ready(function(){
 	setTimeout(function(){
 		document.getElementById("vauban-structurer").style["display"] = "block";
 		let header = document.head.innerHTML
-		document.head.innerHTML = header.replace(`
-			<title>vauban-static</title><link data-n-head="1" rel="icon" type="image/x-icon" href="/favicon.ico"><meta data-n-head="1" charset="utf-8"><meta data-n-head="1" name="viewport" content="width=device-width, initial-scale=1"><meta data-n-head="1" data-hid="description" name="description" content="static components of vauban">
-		`, "")
+
+		header = header.replace(`<title>vauban-static</title>`)
+		header = header.replace(`<link data-n-head="1"rel="icon"type="imagex-icon"href="favicon.ico">`)
+		header = header.replace(`<meta data-n-head="1" charset="utf-8">`)
+		header = header.replace(`<meta data-n-head="1" name="viewport" content="width=device-width initial-scale=1">`)
+		header = header.replace(`<meta data-n-head="1" data-hid="description" name="description" content="static components of vauban">`)
+		document.head.innerHTML = header
 	},1500)  
 });
