d7fd657c8ade4118e9c7ade251df67a87af13343
cyrilpluche <cyril@vauban.io>
2020-01-07 10:49:57 +0000

    remove vauban static

--- inject.js
+++ inject.js
@@ -3,5 +3,9 @@ $(document).ready(function(){
 	$('#vauban-structurer').load("https://cyrilpluche.github.io/");
 	setTimeout(function(){
 		document.getElementById("vauban-structurer").style["display"] = "block";
+		let content = document.getElementById("vauban-structurer").innerHTML
+		document.getElementById("vauban-structurer").innerHTML = html.replace(`
+			<title>vauban-static</title><link data-n-head="1" rel="icon" type="image/x-icon" href="/favicon.ico"><meta data-n-head="1" charset="utf-8"><meta data-n-head="1" name="viewport" content="width=device-width, initial-scale=1"><meta data-n-head="1" data-hid="description" name="description" content="static components of vauban">
+		`, "")
 	},1500)  
 });
